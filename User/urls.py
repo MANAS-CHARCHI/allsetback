37a2cdd5e7d1aef4a0b802e6678971db780f8c01
Manas Charchi <charchimanas2018@gmail.com>
2025-02-16 01:51:43 +0530

    backend, db setup 0.1 15 feb

---
+++ User/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.conf.urls import include 
+
+urlpatterns = [
+    # path('register', ),
+    # path('login',),
+    # path('forget-password', ),
+    # path('logout',),
+    # path('password/reset',),
+    # path('password/reset/verify/<uidb64>/<token>',),
+    # path('password/change',),
+    # path('activate/<uidb64>/<token>',),
+    # path('profile/read',),
+    # path('profile/update',),
+    # path('profile/delete',),
+
+]
